cf56f02ccb05b53fd7cd1fa57240f177688886de
anksv <ankur22388@gmail.com>
2026-06-30 16:03:22 +0200

    feature/drop - Add resize

--- core/src/main/java/io/github/av/game/Main.java
+++ core/src/main/java/io/github/av/game/Main.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
@@ -14,6 +16,9 @@ public class Main extends ApplicationAdapter {
     Sound dropSound;
     Music music;
 
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
     @Override
     public void create() {
         backgroundTexture = new Texture("background.png");
@@ -21,6 +26,14 @@ public class Main extends ApplicationAdapter {
         dropTexture = new Texture("drop.png");
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
     }
 
     @Override
