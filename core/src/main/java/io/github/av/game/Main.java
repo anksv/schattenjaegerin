072462b6eaebe161e8153c5a09a2ace62b0958cb
anksv <ankur22388@gmail.com>
2026-06-30 20:58:32 +0200

    feature/drop - Update input method for movement

--- core/src/main/java/io/github/av/game/Main.java
+++ core/src/main/java/io/github/av/game/Main.java
@@ -2,6 +2,7 @@ package io.github.av.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
@@ -52,7 +53,14 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta); // move the bucket right
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta); // move the bucket left
+        }
     }
 
     private void logic() {
